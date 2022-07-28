2245128b4d457909a99f7beceea1e9061f6348a0
Cristian Barraco <cristian.barraco96@gmail.com>
2022-07-28 15:43:33 +0200

    continuo

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import FeedbackList from "./components/FeedbackList.svelte";
+	import FeedbackStats from "./components/FeedbackStats.svelte";
+	import FeedbackForm from "./components/FeedbackForm.svelte";
 
 	let feedback = [
 		{id:1, name:"John", comment:"Good job!",rating:5},
@@ -7,13 +9,18 @@
 		{id:3, name:"Buggius", comment:"Very bad!",rating:1},
 	]
 
-	// Min 36:18 https://www.youtube.com/watch?v=3TVy6GdtNuQ
+	$: count = feedback.length;
+	$: average = feedback.reduce((a,b) => a+b.rating, 0) / count;
+
 	const deleteFeedback = ({detail}) => {
 		feedback = feedback.filter(f => f.id !== detail)
 	}
 </script>
 
 <main class="container">
+	<FeedbackStats count={count} average={average} />
+	<!-- 44.36 https://www.youtube.com/watch?v=3TVy6GdtNuQ -->
+	<FeedbackForm />
 	<FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 </main>
 
---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,12 @@
+<script>
+
+
+</script>
+
+<form>
+    <div class="input-group">
+        <input type="text" placeholder="Tell us something" />
+        <button >Send</button>
+    </div>
+
+</form>
